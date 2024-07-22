66d4934b98c89c5ee64e7bca78373c7d81c4caa4
tooyond <tooyond@qq.com>
2024-07-22 14:46:30 +0800

    trime 教程中 wubi86_double_key 键盘在 easy_en 输入方案下可用。 教程地址：https://github.com/osfans/trime/wiki/%E4%BA%94%E7%AC%94%E5%8F%8C%E9%94%AE%E9%85%8D%E7%BD%AE%E6%A1%88%E4%BE%8B%E8%AF%A6%E8%A7%A3(%E5%9B%9B)-%E5%AE%9E%E7%8E%B0%E6%89%8B%E6%9C%BA%E4%B8%8A%E7%9A%84%E5%8F%8C%E9%94%AE%E9%94%AE%E7%9B%98

--- trime/adb_connect.bat
+++ trime/adb_connect.bat
@@ -1,4 +1,5 @@
 @echo off
 set/p option= please input ip and port (192.168.0.101:39857):
 
+adb disconnect
 adb connect %option%
--- trime/push.bat
+++ trime/push.bat
@@ -1,5 +1,5 @@
 :: trime.bat
 @REM adb push init_rime/. /sdcard/rime
-adb push rimerc_easy_en/. /sdcard/rime
+@REM adb push rimerc_easy_en/. /sdcard/rime
 adb push rime/. /sdcard/rime
 @REM adb shell am broadcast -a com.osfans.trime.deploy
