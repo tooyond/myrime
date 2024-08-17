f71f7512a5ea679e1635566fc70a36d3a35adbb7
tooyond <tooyond@qq.com>
2024-08-18 00:00:21 +0800

    trime增加了虎码使用的abcdefg布局

--- trime/push.bat
+++ trime/push.bat
@@ -2,6 +2,6 @@
 @REM adb push init_rime/. /sdcard/rime
 @REM adb push rimerc_easy_en/. /sdcard/rime
 @REM adb push wubi86/. /sdcard/rime
-adb push schema_tiger/. /sdcard/rime
+@REM adb push schema_tiger/. /sdcard/rime
 adb push rime/. /sdcard/rime
 @REM adb shell am broadcast -a com.osfans.trime.deploy
